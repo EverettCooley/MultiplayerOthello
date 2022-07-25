af99a6f528bbc5e146106cbb47a5e37580a700bf
Everett <Cooleyeverett4@gmail.com>
2022-07-24 19:02:56 -0700

    Initial Commit

---
+++ MultiplayerOthello/MultiplayerOthelloApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplayerOthelloApp.swift
+//  MultiplayerOthello
+//
+//  Created by v on 7/24/22.
+//
+
+import SwiftUI
+
+@main
+struct MultiplayerOthelloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
